4eabf3cf58c611842d9b7b019ad81e19d926c217
jtkaczyszyn <jtkaczyszyn@elementum.com>
2017-03-23 14:45:30 -0300

    ghpages.js

--- ghpages.js
+++ ghpages.js
@@ -3,9 +3,10 @@ const path    = require('path');
 const repo    = 'https://github.com/pdflegal/pdflegal.github.io.git';
 const branch  = 'master';
 
+console.log(`deploying site to ${repo}#${branch}`);
 ghpages.publish(path.join(__dirname, 'dist'), {
     repo,
     branch
-}, () => {
-    console.log(`deployed site to ${repo}#${branch}`);
+}, (error) => {
+    console.log(`deployement ${error ? 'successfull' : error}`);
 });
