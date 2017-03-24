461d06e2e5e95abfe3a9100b70c07aceffa8071d
facu.tk <facutk@gmail.com>
2017-03-24 20:06:09 -0300

    set git user after git init

--- bin/push-to-ghpages.sh
+++ bin/push-to-ghpages.sh
@@ -7,6 +7,8 @@ CNAME="www.pdflegal.com.ar"
 cd $DIST_FOLDER
 echo "${CNAME}" > CNAME
 git init
+git config user.email "facu.tk@gmail.com"
+git config user.name "facutk"
 git add .
 git commit -m "${CI_COMMIT_ID}"
 git remote add origin $GHPAGES_REPO
