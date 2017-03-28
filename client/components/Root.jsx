65d7b9255e40dacceb096755214ade51a6a0ed75
jtkaczyszyn <jtkaczyszyn@elementum.com>
2017-03-28 19:50:12 -0300

    mock larger interaction test

--- client/components/Root.jsx
+++ client/components/Root.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import fetch from 'isomorphic-fetch';
+
+const addFile = file => {
+    return new Promise((resolve, reject) => {
+        console.log('adding file: ', file);
+        fetch(`${__API__}/add`)
+            .then((response) => {
+                if (response.status >= 400) {
+                    reject('could not add file');
+                }
+                return response.json();
+            })
+            .then(status => resolve(status) );
+    });
+ }
+
+const checkStatus = (resolve, reject) => {
+    console.log('checking status');
+
+    fetch(`${__API__}/status`)
+        .then((response) => {
+            if (response.status >= 400)
+                reject('error processing file');
+            return response.json();
+        })
+        .then(response => {
+            console.log(`status: ${response.status}`);
+            if (response.status == 'error') {
+                reject(response);
+            } else if (response.status == 'processing') {
+                setTimeout(() => checkStatus(resolve, reject), 2000)
+            } else {
+                resolve(response);
+            }
+        });
+
+}
+
+const pollStatus = (status) => {
+    return new Promise((resolve, reject) => {
+        checkStatus(resolve, reject);
+    });
+}
+
+
 
 const Root = () => (
     <div onClick={()=>{
             console.log(__API__);
-            fetch(__API__).then(response => console.log(response));
+
+            addFile('test.pdf')
+                .then(response => pollStatus(response))
+                .then(({status}) => console.log(status))
+                .catch(({status}) => console.error(status));
+
         }}>
         El Reacto 
     </div>
