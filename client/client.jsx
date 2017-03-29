a6c17864884d2e75ecbb2fedb7c6e9d00a2ab4d3
facu.tk <facutk@gmail.com>
2017-03-28 23:06:45 -0300

    redux hello world

--- client/client.jsx
+++ client/client.jsx
@@ -5,6 +5,29 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
 
+
+import { createStore } from 'redux';
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1
+        default:
+            return state;
+    }
+}
+
+let store = createStore(counter)
+
+store.subscribe(() =>
+    console.log(store.getState())
+)
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
+
 render (
     <Root />,
     document.getElementById('root')
