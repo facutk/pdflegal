159b37a0a7e4fcfbb26477ba61d9e6c5d0cb4e87
facu.tk <facutk@gmail.com>
2017-04-01 13:04:35 -0300

    redux inc/dec counter

--- client/client.jsx
+++ client/client.jsx
@@ -9,19 +9,69 @@ import { Provider, connect } from 'react-redux';
 
 import configureStore from './configureStore';
 
-const store = configureStore();
+//const store = configureStore();
 
-store.subscribe(() =>
-    console.log(store.getState())
+//store.subscribe(() =>
+    //console.log(store.getState())
+//)
+
+//store.dispatch({ type: 'INCREMENT' });
+//store.dispatch({ type: 'INCREMENT' });
+//store.dispatch({ type: 'DECREMENT' });
+//store.dispatch({ type: 'ADD_FILE', name: '1.txt' });
+//store.dispatch({ type: 'REMOVE_FILE', name: '1.txt' });
+
+const mapStateToProps = (state) => {
+    return {
+        counter: state.counter
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onIncClick: () => {
+            dispatch({
+                type: 'INCREMENT',
+            })
+        },
+        onDecClick: () => {
+            dispatch({
+                type: 'DECREMENT',
+            })
+        },
+        onFileAdd: () => {
+            dispatch({
+                type: 'ADD_FILE',
+                name: '1.txt'
+            })
+        }
+    };
+};
+
+let CounterApp = (props) => (
+    <div>
+        <ul>
+            <li>
+                <b>Count: {props.counter}</b>
+            </li>
+            <li>
+                <a href="#" onClick={()=>{props.onIncClick()}}>INCREMENT</a>
+            </li>
+            <li>
+                <a href="#" onClick={()=>{props.onDecClick()}}>DECREMENT</a>
+            </li>
+        </ul>
+    </div>
 )
 
-store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'DECREMENT' });
-store.dispatch({ type: 'ADD_FILE', name: '1.txt' });
-store.dispatch({ type: 'REMOVE_FILE', name: '1.txt' });
+CounterApp = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CounterApp)
 
 render (
-    <Root />,
+    <Provider store={configureStore()}>
+        <CounterApp />
+    </Provider>,
     document.getElementById('root')
 );
